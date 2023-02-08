13ab6e52e14eb73ac24569f266816364b5f9db52
girumel <girumel@email.com>
2023-02-08 18:55:59 +0300

    https://www.hackerrank.com/challenges/py-collections-deque

---
+++ python/py-collections-deque.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+n = int(input())
+d = deque()
+for i in range(n):
+    command = input().split()
+    if command[0] == 'append':
+        d.append(int(command[1]))
+    elif command[0] == 'appendleft':
+        d.appendleft(int(command[1]))
+    elif command[0] == 'pop':
+        d.pop()
+    elif command[0] == 'popleft':
+        d.popleft()
+for e in d:
+    print(e, end=' ')
